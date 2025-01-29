395e43266c51823cb29e96bcb686ea8a603aa408
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-01-29 20:10:29 +0530

    feat: env updated

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'https://beyondchat-bot-z5xj.vercel.app/',
     credentials: true
 }))
 
