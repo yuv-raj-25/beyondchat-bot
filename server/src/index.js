1a1bae06d7f8bdf4a8574b8d7cad3594b21df84a
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2025-01-30 10:11:05 +0530

    feat: updated

--- server/src/index.js
+++ server/src/index.js
@@ -16,6 +16,16 @@ app.use(
   );
   
   app.use(cors());
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 app.use(express.json());
 
 app.get('/api/fetchMeta', async (req, res) => {
